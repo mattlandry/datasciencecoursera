a348fc955283a1c6bebaadff98ae1b1ca06a439e
mattlandry <mattlandry2@gmail.com>
2015-06-14 08:41:52 -0500

    Adding complete.R

---
+++ Repo/RProgramming/complete.R
@@ -0,0 +1,35 @@
+##Coursera - RProgramming, Week 2, Assignment 1, Part 2
+##complete.R
+##This function loops over a directory of .csv files and reports the number
+##of complete cases (rowcount, basically)
+
+##Declares the function - 2 variables = directory, range of csv files
+complete <- function(directory, id = 1:332) {
+  
+    ##lists the file names ending with csv
+    filenames <- list.files(path=directory, pattern="*.csv")
+    
+    ##declares holding variables for all ids & counts
+    ids <- vector()
+    counts = vector()
+    
+    ##loops through numbers 1 to 332 (range of id)
+    for(i in id) {
+      ##declares the filenames to be any 3 numbers + csv as an integer
+      filename = sprintf("%03d.csv", i)
+      ##sets the filepath by copying directory & filenames
+      filepath <- paste(directory, filename, sep="/")
+      ##reads the data from each filepath
+      data <- read.csv(filepath)
+      ##loads data into ids
+      ids = c(ids, i)
+      ##uses the complete.cases tool that assesses only observations that have
+      ##no missing values across the entire sequence (row)
+      completeCase = data[complete.cases(data),]
+      ##counts the number of complete cases
+      counts = c(counts, nrow(completeCases))
+    }
+    ##returns the IDs of the csvs and the counts
+    dt <- data.frame(id=ids, nobs=counts)
+    dt
+}
